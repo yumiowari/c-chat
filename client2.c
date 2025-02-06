a5abfec5028e6be3a5bb934c96f38758b6a00722
Yumiowari <owariyumi@gmail.com>
2025-02-06 16:54:12 -0300

    Cliente informando identidade ao servidor

--- client2.c
+++ client2.c
@@ -20,6 +20,7 @@
 #define YELLOW "\x1b[33m"
 
 #define SERVER_IP "127.0.0.1"
+#define BUFFER_SIZE 1024
 
 bool checkArgs(int argc, char **argv);
 // função p/ verificar parâmetros de entrada
@@ -31,9 +32,13 @@ int main(int argc, char **argv){
     char username[16]; // nome de usuário
     int client_socket; // soquete de cliente
     struct sockaddr_in server_addr; // endereço do servidor
+    char buffer[BUFFER_SIZE]; // buffer para I/O
 
     printf("Verificando parâmetros de entrada...\n");
-    if(!checkArgs(argc, argv))exit(EXIT_FAILURE);
+    if(checkArgs(argc, argv)){
+        port = atoi(argv[1]);
+        strcpy(username, argv[2]);
+    }else exit(EXIT_FAILURE);
 
     printf("Criando soquete de cliente...\n");
     client_socket = socket(AF_INET, SOCK_STREAM, 0); // soquete TCP/IPv4
@@ -46,7 +51,6 @@ int main(int argc, char **argv){
     printf("Configurando endereço do servidor...\n");
     server_addr.sin_family = AF_INET;
     server_addr.sin_addr.s_addr = inet_addr(SERVER_IP);
-    port = atoi(argv[1]);
     server_addr.sin_port = htons(port);
 
     printf("Estabelecendo conexão com o servidor...\n");
@@ -55,6 +59,14 @@ int main(int argc, char **argv){
 
         exit(EXIT_FAILURE);
     }else printf(GREEN "\nConexão estabelecida com o servidor!\n" RESET);
+
+    // informa o nome de usuário ao servidor
+    strcpy(buffer, username);
+    if(send(client_socket, buffer, BUFFER_SIZE, 0) == -1){
+        fprintf(stderr, RED "ERRO: Falha ao informar o nome de usuário ao servidor.\n" RESET);
+
+        exit(EXIT_FAILURE);
+    }
 
     exit(EXIT_SUCCESS);
 }
--- server2.c
+++ server2.c
@@ -17,6 +17,14 @@
 #define RED    "\x1b[31m"
 #define GREEN  "\x1b[32m"
 #define YELLOW "\x1b[33m"
+#define BLUE   "\x1b[34m"
+
+#define BUFFER_SIZE 1024
+
+struct client_info{
+    char username[16];
+    int client_socket;
+};
 
 bool checkArgs(int argc, char **argv);
 // função p/ verificar parâmetros de entrada
@@ -27,9 +35,19 @@ int main(int argc, char **argv){
     unsigned short int port; // porta (0 - 65535)
     int server_socket; // soquete do servidor
     struct sockaddr_in server_addr; // endereço do servidor
+    int client_socket; // soquete do cliente
+    struct sockaddr_in client_addr; // endereço do cliente
+    socklen_t client_addr_len = sizeof(client_addr); // tamanho do endereço do cliente
+    char buffer[BUFFER_SIZE]; // buffer para I/O
+    char username[16]; // nome do usuário
+    struct client_info client_id; // identidade do cliente
+    pid_t pid; // "process id"
+    pthread_t tid_in, tid_out; // "thread id"
 
     printf("Verificando parâmetros de entrada...\n");
-    if(!checkArgs(argc, argv))exit(EXIT_FAILURE);
+    if(checkArgs(argc, argv)){
+        port = atoi(argv[1]);
+    }else exit(EXIT_FAILURE);
 
     printf("Criando soquete do servidor...\n");
     server_socket = socket(AF_INET, SOCK_STREAM, 0); // soquete TCP/IPv4
@@ -42,7 +60,6 @@ int main(int argc, char **argv){
     printf("Configurando endereço do servidor...\n");
     server_addr.sin_family = AF_INET;
     server_addr.sin_addr.s_addr = INADDR_ANY; // aceita requisições de qualquer IP
-    port = atoi(argv[1]);
     server_addr.sin_port = htons(port);
 
     printf("Vinculando o soquete do servidor à porta %d...\n", port);
@@ -61,7 +78,27 @@ int main(int argc, char **argv){
 
     // loop do servidor
     while(true){
+        // tenta aceitar uma nova conexão
+        client_socket = accept(server_socket, (struct sockaddr*)&client_addr, &client_addr_len);
+        if(client_socket == -1){
+            fprintf(stderr, RED "ERRO: Falha ao aceitar conexão com um cliente.\n" RESET);
 
+            continue; // tenta novamente
+        }
+
+        // configura a identidade do cliente
+        if(recv(client_socket, buffer, BUFFER_SIZE, 0) <= 0){
+            fprintf(stderr, RED "ERRO: Falha ao receber a identidade do cliente.\n" RESET);
+
+            exit(EXIT_FAILURE);
+        }
+
+        strcpy(username, buffer);
+
+        client_id.client_socket = client_socket;
+        strcpy(client_id.username, username);
+
+        printf(BLUE "%s" RESET " se juntou ao chat!\n", username);
     }
 
     exit(EXIT_SUCCESS);
