65c4d1b1d52b5fce3bfe2ee9706a996ec20501d2
Yumiowari <owariyumi@gmail.com>
2025-04-12 11:16:51 -0300

    Template

---
+++ core/client.c
@@ -0,0 +1,38 @@
+/*
+ *
+ * Sistema de Chatting Cliente-Servidor
+ *
+ * Por Rafael Renó Corrêa, 2025
+ * 
+ * Aplicação Cliente
+ * 
+ */
+
+
+ 
+/*
+ *   Bibliotecas
+ */
+#include <stdlib.h> // exit()
+#include <stdio.h>
+
+/*
+ *   Definições
+ */
+
+/*
+ *   Variáveis Globais
+ */
+
+/*
+ *   Assinaturas
+ */
+
+int main(int argc, char **argv){
+
+    exit(EXIT_SUCCESS);
+}
+
+/*
+ *   Funções
+ */
